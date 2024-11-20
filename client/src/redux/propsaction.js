339e1770ace66568c511c021cf7d3df617c50170
basujeergal <bjeergal@gmail.com>
2024-11-20 22:33:55 +0530

    Update .gitignore to exclude /Documents/webtech

--- client/src/redux/propsaction.js
+++
@@ -1,5 +0,0 @@
-export const NAVCOLOR = "NAVCOLOR";
-export const LOGINCOLOR = "LOGINCOLOR";
-export const ARTISTID = "ARTISTID";
-export const USEREXIT = "USEREXIT";
-export const CANCEL = "CANCEL";
--- client/src/utils/index.js
+++
@@ -1,6 +0,0 @@
-export const convertToMins = (value) => {
-	const mins = Math.floor(value / 60);
-	const secs = Math.round(value - mins * 60, 2);
-	const formattedSeconds = secs < 10 ? "0" + secs : secs;
-	return `${mins}:${formattedSeconds}`;
-};
